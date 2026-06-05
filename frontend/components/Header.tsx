f97bf1377a17bf4c9c2c27cf3d6c6d9a8adde762
Eduardo Gonzalez <agonzalezutn@gmail.com>
2026-06-05 12:51:23 -0300

    tailwind configuration

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,7 +1,10 @@
 export function Header() {
   return (
     <header>
-      <h1>Sistema Gestion Tranfers</h1>
+      <h1 className="bg-purple-600 text-red-100 p-4">Sistema Gestion Tranfers</h1>
+    <p className="bg-purple-600 text-red-900! p-4">Bienvenido al sistema de gestión de transfers. Aquí podrás administrar y organizar tus transfers de manera eficiente.</p>
+
+    
     </header>
   )
 }
