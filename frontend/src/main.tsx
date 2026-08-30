97724bc7596574c912cbf5c33852380622b809db
Eduardo Gonzalez <agonzalezutn@gmail.com>
2026-08-29 21:47:15 -0300

    inicializar entorno frontend y dependencias
    - Instalación de dependencias core: zustand, zod, axios, react-router-dom, react-hook-form y @hookform/resolvers.
    
    - Instalación de dependencias de diseño: @heroicons/react.
    
    - Reestructuración de carpetas siguiendo la arquitectura Feature-Sliced Design (FSD).
    
    - Creación del archivo README.md en la raíz del frontend.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.tsx'
 
-import {Header} from '../components/Header.tsx'
+import {Header} from './components/Header.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
